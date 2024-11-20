49c295795159c72360a5695bcaeb73cf0cd8a973
PuTTY <zien0516@naver.com>
2024-11-20 14:48:59 +0900

    .

--- Assets/02.Script/Common/Manager/UIManager.Find.cs
+++ Assets/02.Script/Common/Manager/UIManager.Find.cs
@@ -18,5 +18,14 @@ public partial class UIManager : MonoBehaviour
         fish1 = Life.GetChild(0).GetComponent<Image>();
         fish2 = Life.GetChild(1).GetComponent<Image>();
         fish3 = Life.GetChild(2).GetComponent<Image>();
+
+        GetSprite();
+    }
+
+    void GetSprite()
+    {
+        fish1.sprite = Sprites.fishImg[0];
+        fish2.sprite = Sprites.fishImg[1];
+        fish3.sprite = Sprites.fishImg[2];
     }
 }
