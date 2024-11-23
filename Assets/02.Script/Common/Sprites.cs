43a8d2d37ca02a428c9d4b38365cc3ee07ea640f
PuTTY <zien0516@naver.com>
2024-11-23 22:56:45 +0900

    .

--- Assets/02.Script/Common/Sprites.cs
+++ Assets/02.Script/Common/Sprites.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public static class Sprites
 {
     public static Sprite[] fishImg;
+    public static Dictionary<Player.LEVEL, Sprite> dieSprites = new Dictionary<Player.LEVEL, Sprite>();
+    public static Dictionary<Player.LEVEL, Sprite> levelSprites = new Dictionary<Player.LEVEL, Sprite>();
 
     static Sprites()
-        => fishImg = Resources.LoadAll<Sprite>("Sprites/fishsheet");
+    {
+        fishImg = Resources.LoadAll<Sprite>("Sprites/fishsheet");
+
+        int[] idx = { 0, 1, 3, 5, 7, 9, 11, 12, 13 };
+        for (int i = 1; i <= 9; i++)
+            levelSprites[(Player.LEVEL)i] = fishImg[idx[i - 1]];
+
+        int[] dieIdx = { 2, 2, 4, 6, 8, 10 };
+        for (int i = 1; i <= 6; i++)
+            dieSprites[(Player.LEVEL)i] = fishImg[dieIdx[i - 1]];
+    }
 }
--- Assets/02.Script/Player/Player.cs
+++ Assets/02.Script/Player/Player.cs
@@ -3,33 +3,20 @@ using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    public enum LEVEL { Level1 = 1, Level2, Level3, Level4, Level5, Level6, Level7, level8, level9 }
-
-    private Dictionary<LEVEL, Sprite> dieSprites = new Dictionary<LEVEL, Sprite>();
+    public enum LEVEL { Level1 = 1, Level2, Level3, Level4, Level5, Level6, Level7, Level8, Level9 }
     
     public LEVEL level = LEVEL.Level1;
     internal SpriteRenderer rend;
 
-    private readonly int[] spriteIdx = { 0, 1, 3, 5, 7, 9, 11, 12, 13 };
-
     void Awake()
-    {
-        dieSprites[LEVEL.Level1] = Sprites.fishImg[2];
-        dieSprites[LEVEL.Level2] = Sprites.fishImg[2];
-        dieSprites[LEVEL.Level3] = Sprites.fishImg[4];
-        dieSprites[LEVEL.Level4] = Sprites.fishImg[6];
-        dieSprites[LEVEL.Level5] = Sprites.fishImg[8];
-        dieSprites[LEVEL.Level6] = Sprites.fishImg[10];
-        
-        rend = GetComponent<SpriteRenderer>();
-    }
+        => rend = GetComponent<SpriteRenderer>();
 
     void Update()
     {
-        if (dieSprites.TryGetValue(level, out Sprite dieSprite))
+        if (Sprites.dieSprites.TryGetValue(level, out Sprite dieSprite))
             GameManager.UI.UpdateLife(rend.sprite, dieSprite);
     }
     
     public void LevelSprite()
-        => rend.sprite = Sprites.fishImg[spriteIdx[(int)level - 1]];
+        => rend.sprite = Sprites.levelSprites[level];
 }
